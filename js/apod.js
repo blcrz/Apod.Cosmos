a11ee1152e1da5179d444d623fac410a9eb2fe59
Bárbara Cruz <ba.luizacruz@gmail.com>
2021-09-06 14:32:32 -0300

    atualizações de estilo e de comandos JS

--- js/apod.js
+++ js/apod.js
@@ -1,12 +1,23 @@
 const title = document.querySelector('#titulo');
 const texto = document.querySelector('p')
 const img = document.getElementById('imagem');
-const hidden = document.querySelector('.card');
-const card = document.querySelector('.container')
+const nav = document.querySelector('nav');
+const card = document.querySelector('.info')
+const back = document.querySelector('body')
+const data = document.querySelector('#data')
+data.classList.add('hidden')
 
 
-$('#btn').click(function() {
-    let result =  document.getElementById("date").value;
+
+$('#date').change(function() {
+    apod('date')
+})
+$('#data').change(function() {
+    apod('data')
+})
+
+function apod(id){
+    let result =  document.getElementById(id).value;
     let link = `https://api.nasa.gov/planetary/apod?api_key=gb8N4ADtCncjCg2CPLeJY1nqhQZ0Tch7jbP4T1Yy&date=${result}`
     let resposta = null
     $.ajax({
@@ -18,13 +29,10 @@ $('#btn').click(function() {
             title.innerText = resposta.title
             texto.innerHTML = resposta.explanation
             img.src = resposta.url
-            hidden.style.display = 'none'
+            nav.style.display = 'none'
+            back.style.background = '#000'
             card.classList.remove('hidden')
-
+            data.classList.remove('hidden')
         }
     })
-
-
-})
-
-
+}
